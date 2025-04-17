ebc2d50ec88831cab2e83d9cc23f5fd34c9cb2d8
HAMID <abdulhamidabari652@gmail.com>
2025-04-17 10:49:44 +0100

    front end

--- Login.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const signupForm = document.querySelector('.login-box');
-    signupForm.style.opacity = 0;
-    setTimeout(() => {
-      signupForm.style.transition = 'opacity 1s ease-in-out';
-      signupForm.style.opacity = 1;
-    }, 500);
-  });
